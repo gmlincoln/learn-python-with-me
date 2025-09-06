1b984e0cd2047ff73848e6a13f602812e36fee63
Md Golam Maula <maula03@gmail.com>
2025-09-06 18:18:52 +0600

    python day-1 class

---
+++ day-1/main.py
@@ -0,0 +1,131 @@
+# a = 10
+# b = 5 // single comment
+
+'''
+Multiple 
+Line 
+Comment
+'''
+
+#Variable Declaration and Data Type Identification
+
+
+"""
+a = 10 
+
+print(type(a));
+
+b = 4.50343
+
+print(type(b))
+
+print(a, b)
+
+"""
+
+# name = 'Rubel Islam'
+# age  = 25
+# voter = True
+
+# print('Name: ' , name , "Age: " , age , "Voter: ", voter);
+
+# print(f'Name: {name} Age: {age} Voter: {voter}')
+
+
+# name = input("Enter your name: ")
+
+# print(name)
+
+# Type casting
+# age = int( input('Enter your age: ') ) 
+
+# print(type(age))
+
+# num1 = int(input('Number-1: '))
+# num2 = int(input('Number-2: '))
+
+# print(num1 + num2)
+
+# a , b = 5, 10
+
+# print(a, b)
+
+# a, b = map(int, input('Enter two numbers: ').split())
+
+# print(a + b)
+
+
+# Operators
+
+# + , -, * , / , % , // , **
+
+# a = 15
+# b = 4
+
+# print(f'Addition: {a} + {b} = {a + b}')
+# print(f'Subtraction: {a} - {b} = {a - b}')
+# print(f'Multiplication: {a} * {b} = {a * b}')
+# print(f'Division: {a} / {b} = {a / b}')
+
+# print(f'Modulas: {a} % {b} = {a % b}');
+# print(f'Floor Division: {a} // {b} = {a // b}')
+
+
+# c = 5
+# d = 3
+
+# print(f'Exponentiation: {c} ** {d} = {c ** d}') # 5 * 5 * 5 = 125
+
+# num = 10 
+# # num = num + 5 
+
+# num += 5 # num = num + 5
+
+# num2 = 5
+
+# num2 **=2
+
+# print(num2)
+
+
+# Relational Operators -> == , >, <, >= , <=
+
+# age = 17
+
+# if age >= 18:
+#     print('Eligible for Vote!')
+# else:
+#     print('Not a voter!')
+ 
+
+
+#  and, or, not
+
+
+
+# Conditionals
+
+# num1 = 10
+
+# num2 = 15
+
+
+# if num1 > num2:
+#     print(f'{num1} is bigger than {num2}')
+# else:
+#     print(f'{num2} is bigger than {num1}')
+
+# a , b, c = 10, 15, 18
+
+
+# print(a > b)
+# print(a > c)
+
+# if a > b and a > c:
+#     print(a)
+# elif b or a & b > c:
+#     print(b)
+# else:
+#     print(c)
+
+
