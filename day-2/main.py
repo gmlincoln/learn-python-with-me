2315ed981fc8af2f778b9ad55c3ed4d5a4c51423
Md Golam Maula <maula03@gmail.com>
2025-09-15 19:40:15 +0600

    basic calculator

--- day-2/main.py
+++ day-2/main.py
@@ -1,9 +1,51 @@
 # while
-i = 1; 
-while i < 5:
-    print(i); # 1 2 3 4
-    i += 1; # 2 3 4 5
+# i = 1; 
+# while i < 5:
+#     print(i); # 1 2 3 4
+#     i +=1; # 2 3 4 5
+    
 
-print(f'Outside loop: {i}');
+# print(f'Outside loop: {i}');
 
 # for 
+
+# name = "Rubel Islam"
+
+# for letter in name:
+#     print(letter)
+
+
+# for x in range( 1 , 11, 1):
+#     print(x); 
+
+
+print("=== Calculator ===")
+print("1. For input two numbers")
+print("Enter")
+value = int(input(''))
+
+
+if value == 1:
+    num1, num2 = map(int, input("Enter two number:").split())
+
+    print('Select Operation (eg. +, -, *, /)')
+    operator = input("Enter operator: ")
+    
+    if operator == '+':
+        print(num1 + num2); 
+    
+    elif operator == '-':
+        print(num1 - num2); 
+    
+    elif operator == '*':
+        print(num1 * num2); 
+    
+    elif operator == '/':
+        print(num1 / num2); 
+
+    else:
+        print('Invalid Operator'); 
+
+else:
+    print('Invalid')
+
